a40287d607a64f7e9f4bdd5d39a97b101ff7fa6b
lv li peng <ai15127937740@163.com>
2018-03-23 20:18:20 +0800

    项目更新

--- daoway/routes/index.js
+++ daoway/routes/index.js
@@ -44,15 +44,16 @@ router.get('/item',function(req,res){
 
 //获取
 router.get('/comment',function(req,res){
-   const page = req.query.page || 1;
+   const page = req.query.page  || 0;
+   console.log(page);
    let options={
      sort:'-createtime',
      limit:10,
-     skip:(page-1)*10
+     skip:page*10
    }
   Comment.find({},null,options,function(err,data){
      if(!err){
-       console.log(data)
+       // console.log(data)
        res.send(data)
      }
    })
